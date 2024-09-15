3eb08c285286f1b717389bbd408fc5658d0481a1
AddSoft <add_soft@icloud.com>
2024-09-15 21:32:50 +0200

    header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import Header from './components/header';
+import './App.css';
 
 const App = () => {
   return (
     <div>
+       <Header />
       <h1>Full-Stack Development</h1>
+     
     </div>
   )
 }
