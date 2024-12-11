a3e4c69a20b799590e4f9633f0dbf0ccb8ac5df7
git-elena <rochnyakelena@gmail.com>
2024-12-11 04:53:23 +0100

    hirosaki

--- src/pages/portfolio/PortfolioHirosaki.js
+++ src/pages/portfolio/PortfolioHirosaki.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import '../../../src/pages/css/Portfolio.css';
+// import '../../../src/pages/css/Portfolio.css';
 import '../../../src/pages/css/PortfolioHirosaki.css';
 
 import device00 from '../../images/portfolio/hirosaki/devices/clear-device-00.png'
@@ -12,9 +12,10 @@ const PortfolioHirosaki = () => {
 
     return (
         <div className='portfolio-page page-content hirosaki'>
-            <div className='portfolio-banner'>
+            <section className='portfolio-header'>
+                <h3>Mobile Application</h3>
                 <div className='container'>
-                    <h3>Mobile Application</h3>
+                    
                     <h1 className='banner-title'>HIROSAKI</h1>
                     <h2 className='banner-subtitle'>Sushi House</h2>
 
@@ -25,39 +26,94 @@ const PortfolioHirosaki = () => {
                         <div className='iphone-logo phone-logo'></div>
                     </div>
                 </div>
-            </div>
+            </section>
             <section className='project-overview'>
+                <h3>Project Overview</h3>
                 <div className="container">
-                    <h3>Project Overview</h3>
-                    <table className='project-overview-table'>
-                        <tbody>
-                            <tr>
-                                <td>
-                                    <p>The primary goal of the project was to improve customer interaction and simplify the ordering process.</p>
-                                    <p>The application provides users with a convenient way to browse the menu, place orders, pay online, and stay informed about special offers.</p>
-                                </td>
-                                <td className='td-50'>
-                                    <div className='devices'>
-                                        <div className='device device00'>
-                                            <img src={device00} alt='Device Screenshot' />
-                                        </div>
-                                        <div className='device device02'>
-                                            <img src={device02} alt='Device Screenshot' />
-                                        </div>
-                                    </div>
-                                </td>
-                                <td>
-                                    <p>We designed this product to meet the specific needs of the restaurant and its customers while supporting business growth.</p>
-                                    <p>Additionally, users can view their past orders offline, adding to the convenience of the app.</p>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
+                    <div className='project-overview-table'>
+                        <div className='td'>
+                            <p>The primary goal of the project was to improve customer interaction and simplify the ordering process.</p>
+                            <p>The application provides users with a convenient way to browse the menu, place orders, pay online, and stay informed about special offers.</p>
+                        </div>
+                        <div className='td td-50'>
+                            <div className=' desc devices'>
+                                <div className='device device00'>
+                                    <img src={device00} alt='Device Screenshot' />
+                                </div>
+                                <div className='device device02'>
+                                    <img src={device02} alt='Device Screenshot' />
+                                </div>
+                            </div>
+
+                            <div className='pad devices '>
+                                <div className='device device00'>
+                                    <img src={device00} alt='Device Screenshot' />
+                                </div>
+                                <div className='device device02'>
+                                    <img src={device02} alt='Device Screenshot' />
+                                </div>
+                            </div>
+                        </div>
+                        <div className='td'>
+                            <p>We designed this product to meet the specific needs of the restaurant and its customers while supporting business growth.</p>
+                            <p>Additionally, users can view their past orders offline, adding to the convenience of the app.</p>
+                        </div>
+                    </div>
+
+                    
                 </div>
             </section>
             <section className='project-design'>
-                <div className='container'>
                 <h3>Project Design</h3>
+                <div className='container'>
+                    <div className='container-fonts'>
+                        <div className='fonts'>
+                            <div className='font font-logo'>
+                                <div className='font-title'>
+                                    <div className='font-position'>Logo</div>
+                                    <div className='font-name'>Made In China</div>
+                                </div>
+                                <div className='font-content'>Aa</div>
+                            </div>
+                            <div className='font font-android'>
+                                <div className='font-title'>
+                                    <div className='font-position'>Android</div>
+                                    <div className='font-name'>Roboto</div>
+                                </div>
+                                <div className='font-content'>Aa</div>
+                            </div>
+                            <div className='font font-iphone'>
+                                <div className='font-title'>
+                                    <div className='font-position'>iPhone</div>
+                                    <div className='font-name'>San Francisco</div>
+                                </div>
+                                <div className='font-content'>Aa</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='colors'>
+                        <div className='color font-logo'>
+                            <div className='font-title'>
+                                <div className='font-position'>Logo</div>
+                                <div className='font-name'>Made In China</div>
+                            </div>
+                            <div className='font-content'>Aa</div>
+                        </div>
+                        <div className='font font-android'>
+                            <div className='font-title'>
+                                <div className='font-position'>Android</div>
+                                <div className='font-name'>Roboto</div>
+                            </div>
+                            <div className='font-content'>Aa</div>
+                        </div>
+                        <div className='font font-iphone'>
+                            <div className='font-title'>
+                                <div className='font-position'>iPhone</div>
+                                <div className='font-name'>San Francisco</div>
+                            </div>
+                            <div className='font-content'>Aa</div>
+                        </div>
+                    </div>
                 </div>
             </section>
         </div>
