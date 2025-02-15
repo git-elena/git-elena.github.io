310cffd8ae5197853357dcaeca4e26539bec6178
Olena PS <olena.ps@example.com>
2025-02-15 08:40:10 +0100

    final

--- src/pages/services/appItem/AppTitle.js
+++ src/pages/services/appItem/AppTitle.js
@@ -4,7 +4,7 @@ import React from 'react'
 const AppTitle = ({left, right}) => {
     return (
         <div className='app-title'>
-            <span className='left'>{left}</span>
+            <span className='left'>{left} </span> 
             <span className='right'>{right}</span>
         </div>
     )
